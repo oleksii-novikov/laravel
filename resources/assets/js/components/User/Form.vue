61b4901188b2883fa515583e1ed347ae6d8a8fd7
Oleksii Novikov <hunter@nixsolutions.com>
2017-03-29 16:20:34 +0300

    Display form-feedback

--- resources/assets/js/components/User/Form.vue
+++ resources/assets/js/components/User/Form.vue
@@ -1,23 +1,27 @@
 <template>
-    <form role="form" v-on:submit.prevent="submit" class="form-horizontal">
-        <div class="form-group">
+    <form v-on:submit.prevent="submit" class="form-horizontal">
+        <div v-bind:class="{'has-danger': errors.name}" class="form-group row">
             <label for="name" class="col-md-4 control-label">Name</label>
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <input id="name" type="text" v-model="user.name" required="required" autofocus="autofocus" class="form-control">
+                <div class="form-control-feedback" v-for="error in errors.name">{{error}}</div>
             </div>
         </div>
-        <div class="form-group">
+        <div v-bind:class="{'has-danger': errors.email}" class="form-group row">
             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <input id="email" type="email" v-model="user.email" required="required" autofocus="autofocus" class="form-control">
+                <div class="form-control-feedback" v-for="error in errors.email">{{error}}</div>
             </div>
         </div>
-        <div class="form-group"><label for="password" class="col-md-4 control-label">Password</label>
-            <div class="col-md-6">
+        <div v-bind:class="{'has-danger': errors.password}" class="form-group row">
+            <label for="password" class="col-md-4 control-label">Password</label>
+            <div class="col-md-8">
                 <input id="password" type="password" v-model="user.password" required="required" class="form-control">
+                <div class="form-control-feedback" v-for="error in errors.password">{{error}}</div>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group row">
             <div class="col-md-8 col-md-offset-4">
                 <button type="submit" class="btn btn-primary">Save</button>
             </div>
@@ -27,7 +31,7 @@
 <script>
     export default {
         name: 'user-form',
-        props: ['user'],
+        props: ['user', 'errors'],
         mounted() {
             console.log('Component Form mounted.')
         },
